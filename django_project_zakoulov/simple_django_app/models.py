bb939ba1cc8258c3bbd658815d319cd9ee226fd3
Zakoulov <zakoylov@gmail.com>
2020-03-11 13:10:17 +0300

    Add models

--- django_project_zakoulov/simple_django_app/models.py
+++ django_project_zakoulov/simple_django_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    state_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.manufacturer}, {self.model}"
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    born_date = models.DateField()
+
+
+class CarDriverOwn(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='driver')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car')
+    date_begin_own = models.DateField()
+    date_end_own = models.DateField()
+
+
+class DriverLicense(models.Model):
+    LICENSE_A = ('a', 'A')
+    LICENSE_B = ('b', 'B')
+    LICENSE_C = ('c', 'C')
+    LICENSE_TYPES = [
+        LICENSE_A,
+        LICENSE_B,
+        LICENSE_C
+    ]
+    owner = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='licenses')
+    license_number = models.CharField(max_length=100)
+    date_of_issuing = models.DateField()
+    license_type = models.CharField(
+        max_length=1,
+        choices=LICENSE_TYPES,
+        default=LICENSE_A[0]
+    )
+
